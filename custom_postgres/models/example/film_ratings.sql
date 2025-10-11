4102d30d572ab42ffb71ef0ba21846e3f8c86baf
RohitManvar <Rohitmanvar123@gmail.com>
2025-10-11 14:06:24 +0530

    dbt implementation

--- custom_postgres/models/example/film_ratings.sql
+++ custom_postgres/models/example/film_ratings.sql
@@ -17,4 +17,17 @@ WITH films_with_rating AS (
 
 films_with_actors AS(
     SELECT
+        f.film_id,
+        f.title,
+        STRING_AGG(a.actor_name,',')as actors
+    FROM{{ref('films')}} f 
+    LEFT JOIN {{ref('film_actors')}} fa ON f.film_id = fa.film_id
+    LEFT JOIN {{ref('actors')}} a ON fa.actor_id = a.actor_id
+    GROUP BY f.film_id, f.title  
 )
+
+SELECT 
+    fwf.*,
+    fwa.actors
+FROM films_with_rating fwf
+LEFT JOIN films_with_actors fwa ON fwf.film_id = fwa.film_id
