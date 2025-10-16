5d9393b7a0f61a71fb7e96665de7fd988d2dfb7f
RohitManvar <Rohitmanvar123@gmail.com>
2025-10-16 19:25:50 +0530

    dbt implementation

--- custom_postgres/models/example/film_rating.sql
+++ custom_postgres/models/example/film_rating.sql
@@ -2,7 +2,7 @@ WITH films_with_rating AS (
     SELECT
         film_id,
         title,
-        release_data,
+        release_date,
         price,
         rating,
         user_rating,
@@ -11,19 +11,19 @@ WITH films_with_rating AS (
             WHEN user_rating >= 4.0 THEN 'Good'
             WHEN user_rating >= 3.0 THEN 'Average'
             ELSE 'Poor'
-        END as rating_category
-    FROM {{ref('films')}}
+        END AS rating_category
+    FROM {{ ref('films') }}
 ),
 
-films_with_actors AS(
+films_with_actors AS (
     SELECT
         f.film_id,
         f.title,
-        STRING_AGG(a.actor_name,',')as actors
-    FROM{{ref('films')}} f 
-    LEFT JOIN {{ref('film_actors')}} fa ON f.film_id = fa.film_id
-    LEFT JOIN {{ref('actors')}} a ON fa.actor_id = a.actor_id
-    GROUP BY f.film_id, f.title  
+        STRING_AGG(a.actor_name, ',') AS actors
+    FROM {{ ref('films') }} f
+    LEFT JOIN {{ ref('film_actors') }} fa ON f.film_id = fa.film_id
+    LEFT JOIN {{ ref('actors') }} a ON fa.actor_id = a.actor_id
+    GROUP BY f.film_id, f.title
 )
 
 SELECT 
